5e207944fe314415adef1302e72adf6920a0d32d
AnjaMahler <mahleranja@gmail.com>
2016-04-18 17:26:57 +0100

    initial commit anja.py

---
+++ anja.py
@@ -0,0 +1,15 @@
+import sys
+print 'hello world'
+print 'wie geht es dir?'
+print 'mir geht es gut!' 
+variable = 'greetings'
+print variable
+variable2 = 'to'
+print variable2
+variable3 = 'kieran'
+print variable3
+print variable + ' ' + variable2 + ' ' + variable3 +' '+ '!!'
+
+filename = sys.argv[1]
+print filename
+print 'this file is ' + filename
